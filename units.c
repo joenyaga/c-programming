828d3fd9e7d99fc43e89d5acd7c3bf2f875edcd0
joenyaga <joenyaga64@gmail.com>
2026-09-14 23:29:46 +0300

    Add files via upload

---
+++ units.c
@@ -0,0 +1,23 @@
+/*
+Author:Joe Nyaga
+Reg Number:BCS-05-0071/2026
+Description:Units offered this semester
+Date:14/09/2026
+Version:5
+*/
+
+//pre-processor directive
+#include <stdio.h>
+int main() 
+{
+	printf("Units Offered This Semester:\n");
+	printf("1.Communication Skills\n");
+	printf("2.Mathematical Physics\n");
+	printf("3.Health And Wellness Management\n");
+	printf("4.Introduction To Computers And Internet\n");
+	printf("5.Structured Programming And Algorithms\n");
+	printf("6.Basic Mathematics\n");
+	printf("7.Computational Logic\n");
+	
+	return 0;
+}
